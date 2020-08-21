d6c50e166d28e593a68f1ec2a2564118b7dc6221
split <christian.schilter@gmail.com>
2020-08-21 12:57:21 -0400

    initial commit

---
+++ volcanobird/asteroids/views.py
@@ -0,0 +1,25 @@
+from datetime import datetime, timedelta
+import requests
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+from volcanobird.settings import NASA_NEO_URL, NASA_API_KEY
+
+def index(request):
+    start_date = datetime.now().strftime("%Y-%m-%d")
+    end_date = (datetime.now() + timedelta(days=7)).strftime("%Y-%m-%d")
+    url = f"{NASA_NEO_URL}start_date={start_date}&end_date={end_date}&api_key={NASA_API_KEY}"
+    r = requests.get(url)
+    j_response = r.json()
+    print(j_response['near_earth_objects'][start_date])
+
+    template = loader.get_template('asteroids/index.html')
+    context = {
+        'element_count': j_response['element_count'],
+        'objects_today': j_response['near_earth_objects'][start_date],
+        'start_date': start_date,
+        'end_date': end_date
+    }
+    return HttpResponse(template.render(context, request))
