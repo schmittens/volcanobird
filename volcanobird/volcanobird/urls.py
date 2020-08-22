1abcdcb7574b27209a5401f7a7176961dc75b214
split <christian.schilter@gmail.com>
2020-08-22 18:38:01 -0400

    heroku

--- volcanobird/volcanobird/urls.py
+++ volcanobird/volcanobird/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('asteroids/', include('volcanobird.asteroids.urls'))
+    path('asteroids/', include('asteroids.urls'))
 ]
